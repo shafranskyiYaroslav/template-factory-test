24492237e9fb2e292c76621e275380d91477b3ea
kommunistMaou <kommunistmaou@gmail.com>
2019-06-26 11:45:20 +0300

    Updates

---
+++ precache-manifest.e0b1ecb70d51fe24fc0285823400b557.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b5bdc1def5bdcacbe788df754ab612d",
+    "url": "/template-factory-test/index.html"
+  },
+  {
+    "revision": "c5e89b5b8bdc58fac3f6",
+    "url": "/template-factory-test/static/css/main.14d17d36.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/template-factory-test/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "c5e89b5b8bdc58fac3f6",
+    "url": "/template-factory-test/static/js/main.39ac5c09.chunk.js"
+  },
+  {
+    "revision": "870cc252ea7c83c04fba",
+    "url": "/template-factory-test/static/js/runtime~main.c54ebf3f.js"
+  }
+]);
